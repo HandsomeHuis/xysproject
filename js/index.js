3f64924f69d3796216ec4c722bec05cb6c4bc5fb
HandsomeHuis <853408967@qq.com>
2018-11-26 00:59:04 +0800

    initial commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+/*
+* @Author: admin
+* @Date:   2018-08-22 14:24:44
+* @Last Modified by:   admin
+* @Last Modified time: 2018-08-22 14:27:05
+*/
+$(function(){
+	$('.a li').mouseenter(function(event) {
+		$(this).addClass('active').siblings('li').removeClass('active')
+	});
+
+
+
+
+
+
+
+
+
+
+})
